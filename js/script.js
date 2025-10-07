ce128fb4f3138f3b62e7843630734ba8b6213779
menabil <nabil99cc@gmail.com>
2025-10-07 23:29:25 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,90 @@
+// alert("Check out You are safe");
+console.log("Yes you are");
+
+var pu = "What if";
+console.log(pu);
+
+let wa = "Monday";
+console.log(wa);
+
+const su = "Fail";
+console.log(su);
+
+var plus = 10;
+var nzx = 7;
+console.log(plus + nzx);
+
+let erw = 10;
+let sad = 17;
+console.log(erw + sad);
+
+const pwr = 10;
+const qrn = 70;
+console.log(pwr + qrn);
+
+var x = 10;
+var x = 7000;
+console.log(x);
+
+let y = 10;
+y = 17000;
+console.log(y);
+
+const z = 10;
+console.log(z);
+
+{
+  var x = "www Ali_Baba";
+}
+console.log(x);
+
+{
+  let x = "amazon";
+  console.log(x);
+}
+
+{
+  const x = "MongoBD";
+  console.log(x);
+}
+
+let fan = 20;
+let wit = 30;
+console.log(fan + wit);
+
+let mot = 20;
+let min = 30;
+console.log(mot - min);
+
+let chi = 20;
+let wah = 30;
+console.log(chi * wah);
+
+let say = 20;
+let kee = 30;
+console.log(say / kee);
+
+let eye = "Amar boyosh";
+let ee = 30;
+console.log(eye + ee);
+
+let num = 5;
+
+console.log(num + " " + "X" + " " + 1 + " " + "=" + " " + num * 1);
+console.log(num + " " + "X" + " " + 2 + " " + "=" + " " + num * 2);
+console.log(num + " " + "X" + " " + 3 + " " + "=" + " " + num * 3);
+console.log(num + " " + "X" + " " + 4 + " " + "=" + " " + num * 4);
+console.log(num + " " + "X" + " " + 5 + " " + "=" + " " + num * 5);
+console.log(num + " " + "X" + " " + 6 + " " + "=" + " " + num * 6);
+console.log(num + " " + "X" + " " + 7 + " " + "=" + " " + num * 7);
+console.log(num + " " + "X" + " " + 8 + " " + "=" + " " + num * 8);
+console.log(num + " " + "X" + " " + 9 + " " + "=" + " " + num * 9);
+console.log(num + " " + "X" + " " + 10 + " " + "=" + " " + num * 10);
+
+let aug = 30;
+
+if (aug > 20) {
+  console.log("It is high time ...");
+} else {
+  console.log("No is't");
+}
